4d9ad398f4c911c3e7c0208363d02c3163788ecd
apple <apple>
2025-11-13 12:16:06 +0800

    first commit

---
+++ app/src/main/java/com/example/overseaswhitebag/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.overseaswhitebag
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+import com.clean.toos.R
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
