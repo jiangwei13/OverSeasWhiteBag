e812ff85f8e38887649b9473beef5d00a80dd52c
apple <apple>
2025-11-13 15:36:37 +0800

    MagicDoc v101

---
+++ app/src/main/java/com/example/overseaswhitebag/privacy/SplashActivity.java
@@ -0,0 +1,80 @@
+package com.example.overseaswhitebag.privacy;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.FrameLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.clean.toos.R;
+import com.xian.bc.accounts.ui.ScanMenuActivity;
+
+
+@SuppressLint("CustomSplashScreen")
+public class SplashActivity extends AppCompatActivity {
+
+
+    FrameLayout splashView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        splashView = findViewById(R.id.splashView);
+
+
+//        boolean isAgressment = SPUtil.with(this).load().read("isAgressment", false);
+//        if (!isAgressment) {
+//            showProtocolDialog();
+//        } else {
+//            toMain();
+//        }
+        toMain();
+    }
+
+    private void showProtocolDialog() {
+        ProtocolDialog protocolDialog = new ProtocolDialog(this, R.style.dialog);
+        protocolDialog.show();
+        protocolDialog.setOnProtocolDialogListener(new ProtocolDialog.OnProtocolDialogListener() {
+            @Override
+            public void agree() {
+                SPUtil.with(SplashActivity.this).load().save("isAgressment", true);
+                toMain();
+            }
+
+            @Override
+            public void refuse() {
+                finish();
+            }
+        });
+    }
+
+    private void toMain() {
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent it = new Intent(SplashActivity.this, ScanMenuActivity.class);
+                startActivity(it);
+                finish();
+
+            }
+        },3000);
+
+
+//        AuditAdUtilsNew.Companion.openCSJSplashAd(this, splashView, new AuditAdUtilsNew.onSplashAdListener() {
+//            @Override
+//            public void splashEnd() {
+//                Intent it = new Intent(SplashActivity.this, ScanMenuActivity.class);
+//                startActivity(it);
+//                finish();
+//            }
+//        });
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/overseaswhitebag/privacy/XieYiActivity.java
@@ -0,0 +1,40 @@
+package com.example.overseaswhitebag.privacy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.clean.toos.R;
+
+
+public class XieYiActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_xieyi);
+
+        Intent intent = getIntent();
+        String title = intent.getStringExtra("TITLE").toString();
+        String content = intent.getStringExtra("CONTENT").toString();
+
+        TextView titleTv = (TextView) findViewById(R.id.title_tv);
+        titleTv.setText(title);
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.loadUrl(content);
+
+        ImageView imageView = findViewById(R.id.backIv);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
