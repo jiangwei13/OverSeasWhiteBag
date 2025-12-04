d1c66d1a27c7d0a47be2d87495296a736f01fa40
apple <apple>
2025-12-04 17:13:29 +0800

    修改是否是root方法

--- app/src/main/java/com/example/overseaswhitebag/common/utils/AdjustTokens.kt
+++ app/src/main/java/com/example/overseaswhitebag/common/utils/AdjustTokens.kt
@@ -11,25 +11,25 @@ import com.p.b.common.adjust.AjConstants
 object AdjustTokens {
     fun initAdJustToken(context: Context) {
         //adJust 打点需要初始化
-        val channel: String = WalleChannelReader.getChannel(context,"OL").toString();
+        val channel: String = WalleChannelReader.getChannel(context,"GP").toString();
         Log.d("AdJustTokenAFUtils>>", "当前渠道》$channel")
         when (channel) {
             "GP" ->{
                 AjConstants.adjustAppToken = "2t7vl9zzgfwg";
                 AjConstants.Login = "";
                 AjConstants.Reg = "";
-                AjConstants.topon_ilrd = "";
-                AjConstants.ipu = "";
-                AjConstants.arpu = "";
-                AjConstants.ipu_arpu = "";
-                AjConstants.ipu_ecpm = "";
-                AjConstants.ipu_arpu_ecpm = "";
-                AjConstants.blacklist = "";
-                AjConstants.ad_request = "";
-                AjConstants.ad_filled = "";
-                AjConstants.firebase_request ="";
-                AjConstants.firebase_success = "";
-                AjConstants.adJust_attribution_request = "";
+                AjConstants.topon_ilrd = "n4say1";
+                AjConstants.ipu = "42y41l";
+                AjConstants.arpu = "yyq45f";
+                AjConstants.ipu_arpu = "stmeot";
+                AjConstants.ipu_ecpm = "r4fdpi";
+                AjConstants.ipu_arpu_ecpm = "sljzeg";
+                AjConstants.blacklist = "70imt6";
+                AjConstants.ad_request = "pkp4go";
+                AjConstants.ad_filled = "v752hy";
+                AjConstants.firebase_request ="3qwssu";
+                AjConstants.firebase_success = "ave12u";
+                AjConstants.adJust_attribution_request = "35lamy";
                 AjConstants.adJust_attribution_success ="";
                 AjConstants.activity_app ="";
             }
