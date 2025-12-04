31298385cba33b2a8c261fa01c25c8ff8d2e0f7d
apple <apple>
2025-12-04 18:55:55 +0800

    增加ContentProvider

---
+++ app/src/main/java/com/example/overseaswhitebag/common/utils/MobInitProvider.java
@@ -0,0 +1,45 @@
+package com.example.overseaswhitebag.common.utils;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+
+import kotlin.jvm.internal.Intrinsics;
+
+public final class MobInitProvider extends ContentProvider {
+    @Override // android.content.ContentProvider
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        Intrinsics.checkNotNullParameter(uri, "uri");
+        return 0;
+    }
+
+    @Override // android.content.ContentProvider
+    public String getType(Uri uri) {
+        Intrinsics.checkNotNullParameter(uri, "uri");
+        return null;
+    }
+
+    @Override // android.content.ContentProvider
+    public Uri insert(Uri uri, ContentValues values) {
+        Intrinsics.checkNotNullParameter(uri, "uri");
+        return null;
+    }
+
+    @Override // android.content.ContentProvider
+    public boolean onCreate() {
+        return true;
+    }
+
+    @Override // android.content.ContentProvider
+    public Cursor query(Uri uri,String[] projection,String selection, String[] selectionArgs, String sortOrder) {
+        Intrinsics.checkNotNullParameter(uri, "uri");
+        return null;
+    }
+
+    @Override // android.content.ContentProvider
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        Intrinsics.checkNotNullParameter(uri, "uri");
+        return 0;
+    }
+}
