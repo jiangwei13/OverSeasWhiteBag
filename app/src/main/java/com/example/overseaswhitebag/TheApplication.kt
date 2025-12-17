6306d54c3365cd3d6f06fbdc449954ef8f4be0d3
apple <apple>
2025-12-17 16:16:48 +0800

    ColorHeart 接入广告

--- app/src/main/java/com/example/overseaswhitebag/TheApplication.kt
+++ app/src/main/java/com/example/overseaswhitebag/TheApplication.kt
@@ -1,7 +1,22 @@
 package com.example.overseaswhitebag
 
+import android.app.Activity
 import android.app.Application
+import android.os.Bundle
 import com.example.overseaswhitebag.common.utils.APPContext
+import com.github.gzuliyujiang.oaid.DeviceIdentifier
+import com.meituan.android.walle.WalleChannelReader
+import com.p.b.InitAdAndTj
+import com.p.b.base.BaseApplication
+import com.p.b.base_api_net.base_api_bean.ConfigUtils
+import com.p.b.base_api_net.utils.DeviceUtils
+import com.p.b.common.GAIDUtil
+import com.p.b.common.SPUtils
+import com.p.b.common.context.HookContext
+import com.p.b.pl223.hhoosstt.AdUtils
+import com.p.b.pl223.hhoosstt.CContext
+import com.tencent.mmkv.MMKV
+import java.lang.ref.WeakReference
 
 
 class TheApplication : Application() {
@@ -19,10 +34,46 @@ class TheApplication : Application() {
         super.onCreate()
         insApp = this
         APPContext.setApplication(this)
+        MMKV.initialize(this)
+        init()
+    }
 
+    private fun init(){
+        val channel: String =
+            WalleChannelReader.getChannel(CContext.getApplication(), "GP").toString()
+        SPUtils.setChannel(channel)
+        val defaultConfig: String = ConfigUtils.getConfigJson(CContext.getApplication())
+        ConfigUtils.initConfig(defaultConfig, 1)
+        initActivityListener()
+        DeviceIdentifier.register(this);
+        DeviceUtils.getFetchOaid()
+        GAIDUtil.fetchGAID(this, null)
+
+        InitAdAndTj.initAdTj(insApp)
+        InitAdAndTj.initJumpEvent(insApp)
     }
 
 
 
+    fun initActivityListener() {
+        registerActivityLifecycleCallbacks(object : ActivityLifecycleCallbacks {
+            override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+                HookContext.appCompatActivity = WeakReference(activity)
+                if (AdUtils.isAdActivity(activity)) {
+                    CContext.initCurrAdActivity(WeakReference(activity))
+                }
+            }
 
+            override fun onActivityStarted(activity: Activity) {}
+            override fun onActivityResumed(activity: Activity) {}
+            override fun onActivityPaused(activity: Activity) {}
+            override fun onActivityStopped(activity: Activity) {}
+            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
+            override fun onActivityDestroyed(activity: Activity) {
+                if (AdUtils.isAdActivity(activity)) {
+                    CContext.removeAdActivity(WeakReference(activity))
+                }
+            }
+        })
+    }
 }
