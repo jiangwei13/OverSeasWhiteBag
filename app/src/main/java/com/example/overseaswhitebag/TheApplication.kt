f0ed210b1a61f81d3a4d468ff23ee6f2bdf62360
apple <apple>
2025-12-20 16:26:04 +0800

    v106增加 firebase 和adjust

--- app/src/main/java/com/example/overseaswhitebag/TheApplication.kt
+++ app/src/main/java/com/example/overseaswhitebag/TheApplication.kt
@@ -3,23 +3,38 @@ package com.example.overseaswhitebag
 import android.app.Activity
 import android.app.Application
 import android.os.Bundle
+import android.util.Log
 import com.example.overseaswhitebag.common.utils.APPContext
+import com.example.overseaswhitebag.common.utils.AdjustTokens
 import com.github.gzuliyujiang.oaid.DeviceIdentifier
+import com.google.firebase.FirebaseApp
 import com.meituan.android.walle.WalleChannelReader
 import com.p.b.InitAdAndTj
 import com.p.b.base.BaseApplication
 import com.p.b.base_api_net.base_api_bean.ConfigUtils
 import com.p.b.base_api_net.utils.DeviceUtils
+import com.p.b.base_api_net.utils.HandleUtils
 import com.p.b.common.GAIDUtil
+import com.p.b.common.MMKVUtils
+import com.p.b.common.OverseaAppContext
+import com.p.b.common.PhoneStatusUtils
 import com.p.b.common.SPUtils
+import com.p.b.common.adjust.AdJustInitUtils
+import com.p.b.common.adjust.AdJustTokenAFUtils.doActivateDot
+import com.p.b.common.adjust.AjConstants
+import com.p.b.common.adjust.CommonConfig
 import com.p.b.common.context.HookContext
+import com.p.b.common.doOnMainThreadIdle
+import com.p.b.common.fcm.FCMInitUtils
+import com.p.b.common.firebase.FireBaseInitUtils
+import com.p.b.http.HostUtils
 import com.p.b.pl223.hhoosstt.AdUtils
 import com.p.b.pl223.hhoosstt.CContext
 import com.tencent.mmkv.MMKV
 import java.lang.ref.WeakReference
 
 
-class TheApplication : Application() {
+class TheApplication : BaseApplication() {
 
     companion object {
         var isBackLanch: Boolean = false
@@ -27,7 +42,34 @@ class TheApplication : Application() {
         @JvmStatic
         var insApp: TheApplication? = null
 
+        @JvmStatic
+        var fromNet:Runnable = Runnable {
+            if(SPUtils.isUserCommon()){
+                return@Runnable
+            }
 
+            //Adjust归因
+            AdJustInitUtils.initAdjust(HostUtils.randomConfig_from_delay,
+                AjConstants.adjustAppToken,
+                false,
+                object :CommonConfig.OnConfigInterface{
+                    override fun onSuccess() {
+                        //归因状态
+                        MMKVUtils.setUserStatus(true)
+                        //拉取数据
+                        FireBaseInitUtils.fetchData(HostUtils.randomConfig_from_delay)
+//                        doOnMainThreadIdle({
+//                            InitAdAndTj.initJumpEvent(insApp)
+//                        })
+                    }
+
+                    override fun onFail() {
+                        MMKVUtils.setUserStatus(false)
+                    }
+
+                }
+                )
+        }
     }
 
     override fun onCreate() {
@@ -35,7 +77,12 @@ class TheApplication : Application() {
         insApp = this
         APPContext.setApplication(this)
         CContext.setApplication(this)
+        OverseaAppContext.setApplication(this)
         MMKV.initialize(this)
+        // 初始化Firebase
+        FirebaseApp.initializeApp(this)
+        // 初始化FCM
+        FCMInitUtils.init(this)
         init()
     }
 
@@ -45,13 +92,19 @@ class TheApplication : Application() {
         SPUtils.setChannel(channel)
         val defaultConfig: String = ConfigUtils.getConfigJson(CContext.getApplication())
         ConfigUtils.initConfig(defaultConfig, 1)
+        AdjustTokens.initAdJustToken(this)
         initActivityListener()
+        adJustCheckUpload()
+
         DeviceIdentifier.register(this);
         DeviceUtils.getFetchOaid()
         GAIDUtil.fetchGAID(this, null)
+         if(isStartWork()){
+             Log.d("AD_LOG", "初始化广告sdk")
+             InitAdAndTj.initAdTj(insApp)
+             HandleUtils.postDelay(fromNet,10*1000)
+         }
 
-        InitAdAndTj.initAdTj(insApp)
-        //InitAdAndTj.initJumpEvent(insApp)
     }
 
 
@@ -76,5 +129,8 @@ class TheApplication : Application() {
                 }
             }
         })
+    }
+    fun adJustCheckUpload() {
+        doActivateDot()
     }
 }
