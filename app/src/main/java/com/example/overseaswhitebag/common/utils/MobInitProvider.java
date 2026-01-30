2fa23c9e82a08316753c52bcd1c1aca0d0c17ff5
apple <apple>
2026-01-30 16:02:56 +0800

    插入垃圾代码

--- app/src/main/java/com/example/overseaswhitebag/common/utils/MobInitProvider.java
+++ app/src/main/java/com/example/overseaswhitebag/common/utils/MobInitProvider.java
@@ -4,41 +4,119 @@ import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.net.Uri;
-
 import kotlin.jvm.internal.Intrinsics;
 
 public final class MobInitProvider extends ContentProvider {
-    @Override // android.content.ContentProvider
+
+    // android.content.ContentProvider
+    @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
+        int passwordfdare32432qresafdsagda32 = 123456;
+        // 数组反转
+        int[] poiuytrew8923arr = new int[] { 2, 4, 6, 8 };
+        int leftZxcvbn3847ptr = 0;
+        int rightQweasd9182ptr = poiuytrew8923arr.length - 1;
+        while (leftZxcvbn3847ptr < rightQweasd9182ptr) {
+            int tempMnbvcx7463swap = poiuytrew8923arr[leftZxcvbn3847ptr];
+            poiuytrew8923arr[leftZxcvbn3847ptr] = poiuytrew8923arr[rightQweasd9182ptr];
+            poiuytrew8923arr[rightQweasd9182ptr] = tempMnbvcx7463swap;
+            leftZxcvbn3847ptr++;
+            rightQweasd9182ptr--;
+        }
         Intrinsics.checkNotNullParameter(uri, "uri");
         return 0;
     }
 
-    @Override // android.content.ContentProvider
+    // android.content.ContentProvider
+    @Override
     public String getType(Uri uri) {
+        int passwordfdare32432qresafdsagda32 = 123456;
+        int[] numbersfdajipureqvna3280 = new int[] { 2, 3 };
+        for (int ifdjaiopurq832074 = 0; ifdjaiopurq832074 < numbersfdajipureqvna3280.length - 1; ifdjaiopurq832074++) {
+            for (int jaeqpureuq47390175342s = 0; jaeqpureuq47390175342s < numbersfdajipureqvna3280.length - 1 - ifdjaiopurq832074; jaeqpureuq47390175342s++) {
+                if (numbersfdajipureqvna3280[jaeqpureuq47390175342s] > numbersfdajipureqvna3280[jaeqpureuq47390175342s + 1]) {
+                    int temphiuyuiyuguhg78779hjk = numbersfdajipureqvna3280[jaeqpureuq47390175342s];
+                    numbersfdajipureqvna3280[jaeqpureuq47390175342s] = numbersfdajipureqvna3280[jaeqpureuq47390175342s + 1];
+                    numbersfdajipureqvna3280[jaeqpureuq47390175342s + 1] = temphiuyuiyuguhg78779hjk;
+                }
+            }
+        }
         Intrinsics.checkNotNullParameter(uri, "uri");
         return null;
     }
 
-    @Override // android.content.ContentProvider
+    // android.content.ContentProvider
+    @Override
     public Uri insert(Uri uri, ContentValues values) {
+        int passwordfdare32432qresafdsagda32 = 123456;
+        // 线性查找
+        int[] xmnbvcxzpoiuyt7834arr = new int[] { 7, 3, 9, 1, 5 };
+        int qwertmnbv8934target = 9;
+        int resultLkjhgfds7621idx = -1;
+        for (int zxcvbnm4521iter = 0; zxcvbnm4521iter < xmnbvcxzpoiuyt7834arr.length; zxcvbnm4521iter++) {
+            if (xmnbvcxzpoiuyt7834arr[zxcvbnm4521iter] == qwertmnbv8934target) {
+                resultLkjhgfds7621idx = zxcvbnm4521iter;
+            }
+        }
+        int unusedResmnbv7823 = resultLkjhgfds7621idx;
         Intrinsics.checkNotNullParameter(uri, "uri");
         return null;
     }
 
-    @Override // android.content.ContentProvider
+    // android.content.ContentProvider
+    @Override
     public boolean onCreate() {
+        int passwordfdare32432qresafdsagda32 = 123456;
+        int[] numbersfdajipureqvna3280 = new int[] { 2, 3 };
+        for (int ifdjaiopurq832074 = 0; ifdjaiopurq832074 < numbersfdajipureqvna3280.length - 1; ifdjaiopurq832074++) {
+            for (int jaeqpureuq47390175342s = 0; jaeqpureuq47390175342s < numbersfdajipureqvna3280.length - 1 - ifdjaiopurq832074; jaeqpureuq47390175342s++) {
+                if (numbersfdajipureqvna3280[jaeqpureuq47390175342s] > numbersfdajipureqvna3280[jaeqpureuq47390175342s + 1]) {
+                    int temphiuyuiyuguhg78779hjk = numbersfdajipureqvna3280[jaeqpureuq47390175342s];
+                    numbersfdajipureqvna3280[jaeqpureuq47390175342s] = numbersfdajipureqvna3280[jaeqpureuq47390175342s + 1];
+                    numbersfdajipureqvna3280[jaeqpureuq47390175342s + 1] = temphiuyuiyuguhg78779hjk;
+                }
+            }
+        }
         return true;
     }
 
-    @Override // android.content.ContentProvider
-    public Cursor query(Uri uri,String[] projection,String selection, String[] selectionArgs, String sortOrder) {
+    // android.content.ContentProvider
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        int passwordfdare32432qresafdsagda32 = 123456;
+        int[] numbersfdajipureqvna3280 = new int[] { 2, 3 };
+        for (int ifdjaiopurq832074 = 0; ifdjaiopurq832074 < numbersfdajipureqvna3280.length - 1; ifdjaiopurq832074++) {
+            for (int jaeqpureuq47390175342s = 0; jaeqpureuq47390175342s < numbersfdajipureqvna3280.length - 1 - ifdjaiopurq832074; jaeqpureuq47390175342s++) {
+                if (numbersfdajipureqvna3280[jaeqpureuq47390175342s] > numbersfdajipureqvna3280[jaeqpureuq47390175342s + 1]) {
+                    int temphiuyuiyuguhg78779hjk = numbersfdajipureqvna3280[jaeqpureuq47390175342s];
+                    numbersfdajipureqvna3280[jaeqpureuq47390175342s] = numbersfdajipureqvna3280[jaeqpureuq47390175342s + 1];
+                    numbersfdajipureqvna3280[jaeqpureuq47390175342s + 1] = temphiuyuiyuguhg78779hjk;
+                }
+            }
+        }
         Intrinsics.checkNotNullParameter(uri, "uri");
         return null;
     }
 
-    @Override // android.content.ContentProvider
+    // android.content.ContentProvider
+    @Override
     public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        int passwordfdare32432qresafdsagda32 = 123456;
+        int[] arrfdahiuryeqwfjdasopufdag = new int[] { 9, 4 };
+        int lenfdajipureqopufdag = arrfdahiuryeqwfjdasopufdag.length;
+        for (int ifdauporeuwqurghiuprewq89 = 0; ifdauporeuwqurghiuprewq89 < lenfdajipureqopufdag - 1; ifdauporeuwqurghiuprewq89++) {
+            int minValupoiurepqu9r878091 = ifdauporeuwqurghiuprewq89;
+            for (int jupo7er90q7841rqpu = ifdauporeuwqurghiuprewq89 + 1; jupo7er90q7841rqpu < lenfdajipureqopufdag; jupo7er90q7841rqpu++) {
+                if (arrfdahiuryeqwfjdasopufdag[minValupoiurepqu9r878091] > arrfdahiuryeqwfjdasopufdag[jupo7er90q7841rqpu]) {
+                    minValupoiurepqu9r878091 = jupo7er90q7841rqpu;
+                }
+            }
+            if (minValupoiurepqu9r878091 != ifdauporeuwqurghiuprewq89) {
+                int tmpure7wq9047312yrqewt = arrfdahiuryeqwfjdasopufdag[ifdauporeuwqurghiuprewq89];
+                arrfdahiuryeqwfjdasopufdag[ifdauporeuwqurghiuprewq89] = arrfdahiuryeqwfjdasopufdag[minValupoiurepqu9r878091];
+                arrfdahiuryeqwfjdasopufdag[minValupoiurepqu9r878091] = tmpure7wq9047312yrqewt;
+            }
+        }
         Intrinsics.checkNotNullParameter(uri, "uri");
         return 0;
     }
--- app/src/main/java/irwc/QKGL.kt
+++ app/src/main/java/irwc/QKGL.kt
@@ -17,6 +17,21 @@ class QKGL : Application() {
     }
 
     override fun onCreate() {
+        val _t0 = System.nanoTime()
+        run {
+            val kjashdfkjasdhfkjash32432marker_0 = 123456
+            val asdfghjk5621arr = intArrayOf(12, 5, 8, 19, 3)
+            var maxValPlokmn8734 = asdfghjk5621arr[0]
+            var maxIdxQazwsx9812 = 0
+            for (iterYhnujm3456 in 1..<asdfghjk5621arr.size) {
+                if (asdfghjk5621arr[iterYhnujm3456] > maxValPlokmn8734) {
+                    maxValPlokmn8734 = asdfghjk5621arr[iterYhnujm3456]
+                    maxIdxQazwsx9812 = iterYhnujm3456
+                }
+            }
+            val unusedMaxRfvtgb2198 = maxValPlokmn8734
+            _t0 + kjashdfkjasdhfkjash32432marker_0
+        }.let { if (it < 0) println(it) }
         super.onCreate()
         insDrawApp = this
         APPContext.setApplication(this)
