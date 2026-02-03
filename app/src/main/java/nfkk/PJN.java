bfee07fd3bf37d8b4870e6f63be1e9d460bd2d38
edy <1165158252@qq.com>
2026-02-03 14:33:08 +0800

    新增 沈阳-海外led 星梦LED工坊

---
+++ app/src/main/java/nfkk/PJN.java
@@ -0,0 +1,40 @@
+package nfkk;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import tmlr.hti.caqq.R;
+
+
+public class PJN extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_xieyi);
+
+        Intent intent = getIntent();
+        String title = intent.getStringExtra("TITLE").toString();
+        String content = intent.getStringExtra("CONTENT").toString();
+
+        TextView titleTv = (TextView) findViewById(R.id.title_tv);
+        titleTv.setText(title);
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.loadUrl(content);
+
+        ImageView imageView = findViewById(R.id.backIv);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
