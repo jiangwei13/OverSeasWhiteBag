979ddf0c0a6736d3c74609aa490a2565e71617ac
flyun <fylysun@gmail.com>
2026-03-24 17:02:26 +0800

    删除无用项目

--- test_app/src/main/java/com/example/overseaswhitebag/TheApplication.kt
+++
@@ -1,9 +0,0 @@
-package com.example.overseaswhitebag
-
-import android.app.Application
-
-class TheApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
-    }
-}
--- test_app/src/main/java/com/example/overseaswhitebag/common/utils/DemoUtils.kt
+++
@@ -1,7 +0,0 @@
-package com.example.overseaswhitebag.common.utils
-
-object DemoUtils {
-    fun getVersion(): String {
-        return "1.0.0"
-    }
-}
