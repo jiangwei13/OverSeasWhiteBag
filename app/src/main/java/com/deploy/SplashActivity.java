c26ffea37ebc2f53cfd7c72860d852df5c97c4fb
flyun <fylysun@gmail.com>
2026-05-26 14:47:51 +0800

    关闭tab广告

--- app/src/main/java/com/deploy/SplashActivity.java
+++ app/src/main/java/com/deploy/SplashActivity.java
@@ -5,10 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.FrameLayout;
-import androidx.appcompat.app.AppCompatActivity;
-import com.deploy.R;
-import com.p.b.ad.AdViewMana;
+
 import com.fangda.MainTwoActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 @SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
@@ -24,7 +24,7 @@ public class SplashActivity extends AppCompatActivity {
     }
 
     private void toMain() {
-        AdViewMana.initView(this, "in_tab");
+//        AdViewMana.initView(this, "in_tab");
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
