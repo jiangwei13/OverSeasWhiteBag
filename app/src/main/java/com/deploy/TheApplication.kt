20fa445becfc77f3c73e88451a42d3f3581ecb92
flyun <fylysun@gmail.com>
2026-06-23 10:42:16 +0800

    集成广告

--- app/src/main/java/com/deploy/TheApplication.kt
+++ app/src/main/java/com/deploy/TheApplication.kt
@@ -1,12 +1,13 @@
 package com.deploy
 
-import android.app.Application
 import android.content.Intent
 import android.os.Bundle
 import com.deploy.tools.ToolUiInstaller
+import com.p.b.base.APPContext
+import com.p.b.base.BaseApplication
 
 
-class TheApplication : Application() {
+class TheApplication : BaseApplication() {
 
     companion object {
         @JvmStatic
@@ -17,25 +18,25 @@ class TheApplication : Application() {
         super.onCreate()
         insApp = this
         // 保证白包有 context
-//        APPContext.setApplication(this)
+        APPContext.setApplication(this)
         // 统一安装各工具页的自定义 UI(按 ToolType 区分;未注册的工具用功能层默认 UI)
         ToolUiInstaller.installAll()
     }
 
-//    override fun configureAdjustTokens() {
-//        AdjustTokens.initAdJustToken(this)
-//    }
-//
-//    override fun initPopPower() {
-//        TODO("Not yet implemented")
-//    }
-//
-//    override fun initKeepPower() {
-//        TODO("Not yet implemented")
-//    }
-//
-//    override fun openLaunchByOther(bundle: Bundle?, intent: Intent) {
-//        // 由其他 App 拉起时，转交中转页处理
-//    }
+    override fun configureAdjustTokens() {
+        AdjustTokens.initAdJustToken(this)
+    }
+
+    override fun initPopPower() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initKeepPower() {
+        TODO("Not yet implemented")
+    }
+
+    override fun openLaunchByOther(bundle: Bundle?, intent: Intent) {
+        // 由其他 App 拉起时，转交中转页处理
+    }
 
 }
